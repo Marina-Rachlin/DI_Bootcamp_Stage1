cf93abea610fcaefa9119ac0747b96188fb2006a
Marina Rachlin <Marina.rachlin@gmail.com>
2023-05-23 01:51:19 +0300

    W4D2 finished

---
+++ week4/day2/Dailychallenge/Dailyhallenge.sql
@@ -0,0 +1,50 @@
+-- CREATE TABLE FirstTab (
+--      id integer, 
+--      name VARCHAR(10)
+-- );
+
+-- INSERT INTO FirstTab VALUES
+-- (5,'Pawan'),
+-- (6,'Sharlee'),
+-- (7,'Krish'),
+-- (NULL,'Avtaar');
+
+-- SELECT * FROM FirstTab;
+
+-- CREATE TABLE SecondTab (
+--     id integer 
+-- );
+
+-- INSERT INTO SecondTab VALUES
+-- (5),
+-- (NULL);
+
+-- Questions
+-- Q1. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NULL )
+-- Comparing against NUll always returns 0!
+
+-- Output: 0
+
+
+-- Q2. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id = 5 )
+-- Output: 2
+
+-- Q3. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab )
+-- Output: 0
+-- Comparing against NUll always returns 0!
+
+
+-- Q4. What will be the OUTPUT of the following statement?
+
+--     SELECT COUNT(*) 
+--     FROM FirstTab AS ft WHERE ft.id NOT IN ( SELECT id FROM SecondTab WHERE id IS NOT NULL )
+-- Output: 2
