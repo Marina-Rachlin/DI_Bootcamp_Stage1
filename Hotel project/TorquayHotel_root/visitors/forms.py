256e145b6c19096aa2b049e5ff05a137503d0146
Marina Rachlin <Marina.rachlin@gmail.com>
2023-06-22 10:57:30 +0300

    w7d5 exerciseXP done

--- Hotel project/TorquayHotel_root/visitors/forms.py
+++ Hotel project/TorquayHotel_root/visitors/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
+# from django.contrib.auth.forms import AuthenticationForm
 from .models import *
 
 
@@ -16,11 +16,24 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = '__all__'
+        
 
 class CheckAvailabilityForm(forms.Form):
     check_in_date = forms.DateField()
     check_out_date = forms.DateField()
-    num_guests = forms.IntegerField()        
+    num_guests = forms.IntegerField()   
+
+
+class GuestDetailsForm(forms.Form):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
+    phone = forms.CharField(max_length=20)
+    email = forms.EmailField()
+    country = forms.CharField(max_length=100)
+    city = forms.CharField(max_length=100)
+    zip_code = forms.CharField(max_length=20)
+    additional_details = forms.CharField(widget=forms.Textarea)
+    preferences = forms.CharField(widget=forms.Textarea)     
 
 
 # class BookingForm(forms.Form):
---
+++ week7/day5/exerciseXP/script.js
@@ -0,0 +1,59 @@
+// Function to generate a random number between min and max (inclusive)
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;// we can do it like this : Math.floor(Math.random() * 11)
+  }
+  
+  // Function to compare the user's number with the computer's number
+  function compareNumbers(userNumber, computerNumber) {
+    let counter = 0;
+  
+    while (counter < 3) {
+      if (userNumber === computerNumber) {
+        alert("WINNER");
+        return; // Stop the game
+      }
+  
+      if (userNumber > computerNumber) {
+        alert("Your number is bigger than the computer’s, guess again");
+      } else {
+        alert("Your number is smaller than the computer’s, guess again");
+      }
+  
+      counter++;
+      userNumber = parseInt(prompt("Enter a new number:"));
+    }
+  
+    alert("Out of chances");
+  }
+   
+  // Function to play the guessing game
+  function playTheGame() {
+    const playGame = confirm("Would you like to play the game?");
+  
+    if (!playGame) {
+      alert("No problem, Goodbye.");
+    } else {
+      let userNumber;
+  
+      while (true) {
+        userNumber = parseInt(prompt("Enter a number between 0 and 10:"));
+  
+        if (isNaN(userNumber)) {
+          alert("Sorry, not a number. Try again.");
+        } else if (userNumber < 0 || userNumber > 10) {
+          alert("Sorry, it's not a good number. Try again.");
+        } else {
+          break;
+        }
+      }
+  
+      const computerNumber = getRandomNumber(0, 10);
+      compareNumbers(userNumber, computerNumber);
+    }
+  }
+  
+  // Call the playTheGame function when the button is clicked
+  function handleClick() {
+    playTheGame();
+  }
+  
