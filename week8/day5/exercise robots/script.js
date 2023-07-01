91be6e42b731e85a962de2e1ef009f5082183361
Marina Rachlin <Marina.rachlin@gmail.com>
2023-07-02 01:09:41 +0300

    w8d5 exerciseXp-robots done

---
+++ week8/day5/exercise robots/script.js
@@ -0,0 +1,111 @@
+const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+    ];
+
+
+// part 1
+function createRobotCard(robot){
+    const html = `
+        <img class="card-img-top" src="${robot['image']}">
+        <div class="card-body">
+            <h5 class="card-title">${robot['name']}</h5>
+            <p class="card-text">${robot['email']}</p>
+        </div>
+  `;
+  let card = document.createElement('div');
+  card.classList.add('card');
+  card.style.width='18rem';
+  card.innerHTML = html;
+  return card;
+}
+
+const renderRobotList = robots => robots.forEach(robot => {document.getElementById('content').appendChild(createRobotCard(robot))});
+
+renderRobotList(robots);
+
+// part 2
+const input = document.getElementById('search');
+input.addEventListener('input', () => {
+    let results = robots.filter(element => element['name'].toLowerCase().includes(input.value.toLowerCase()));
+    content.innerHTML = '';
+    renderRobotList(results);
+})
+
+
+
+
+  
+
+
+
+
+
